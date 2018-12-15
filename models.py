c3ad522eefbfaef0ce6f7fb8715e921541426448
ZZH-ubuntu-office <741788769@qq.com>
2018-12-15 21:45:52 +0800

    My first DeepLearning program: Facial Keypoints detection

--- models.py
+++ models.py
@@ -8,7 +8,53 @@ import torch.nn.functional as F
 import torch.nn.init as I
 
 
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1   
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)   # (N, C, H, W) 在C（feature map的个数）维度上进行BN，N为batch-size的大小
+        self.relu = nn.ReLU(inplace=True)   #inplace为True，将会改变输入的数据 ，否则不会改变原输入，只会产生新的输出
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample        # 改变特征图的个数
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x        # 参考的ResNet？？？
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual     
+        out = self.relu(out)
+
+        return out
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.shape[0], -1)   # 将特征平展成一行
+
+
+
+
+
+
 class Net(nn.Module):
+    inplanes = 64   # 网络搭建过程中每一block的特征图的输入个数
 
     def __init__(self):
         super(Net, self).__init__()
@@ -20,11 +66,52 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        #self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(True)
+        self.maxpool = nn.MaxPool2d(3, 2, 1)
+        self.layer1 = self._make_layer(BasicBlock, 64, 2)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))  # 每个map上只留一个特征了
+        self.fc = nn.Sequential(
+            Flatten(),
+            nn.Linear(512, 136)
+        )
+
+        # initialize
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            # 使用卷积完成采样(可能改变的是核数，可能改变的是尺寸数)
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes*block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes*block.expansion),
+            )
+
+        layer = []
+        layer.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layer.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layer)
+
 
         
     def forward(self, x):
@@ -32,6 +119,18 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = self.fc(x)
+
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
